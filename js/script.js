4205a8962e089929e63fd125b727a9a3c857e069
Simon Reinhardt <saadabou.study@gmail.com>
2022-06-25 13:27:14 +0100

    BG color change and Header fix

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-// var preloaderTimer = setInterval(myTimer, 4000);
-// function myTimer() {
-//     var loader = document.getElementById("container");
-//     loader.style.display = "none";
-// }
+var preloaderTimer = setInterval(myTimer, 4000);
+function myTimer() {
+    var loader = document.getElementById("container");
+    loader.style.display = "none";
+}
