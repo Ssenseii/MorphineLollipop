18129451b25d0cc756692ffec5d37dec2d389714
Simon Reinhardt <saadabou.study@gmail.com>
2022-06-26 10:11:11 +0100

    loading screen done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
-var preloaderTimer = setInterval(myTimer, 4000);
-function myTimer() {
+var preloaderTimer = setInterval(preloaderTimer, 4000);
+function preloaderTimer() {
     var loader = document.getElementById("container");
     loader.style.display = "none";
+}
+
+var loadEverything = setInterval(Timer, 4000);
+var loading = document.getElementById("load");
+loading.style.display = "none";
+function Timer(){
+    var loading = document.getElementById("load");
+    loading.style.display = "initial";
 }
