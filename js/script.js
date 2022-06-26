6d26dd302a819bc3afd0fa7aa5c559664a7811bc
Simon Reinhardt <saadabou.study@gmail.com>
2022-06-26 11:06:41 +0100

    finished main page and header

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-var preloaderTimer = setInterval(preloaderTimer, 4000);
+var preloaderTimer = setInterval(preloaderTimer, 1000);
 function preloaderTimer() {
     var loader = document.getElementById("container");
     loader.style.display = "none";
 }
 
-var loadEverything = setInterval(Timer, 4000);
+var loadEverything = setInterval(Timer, 1000);
 var loading = document.getElementById("load");
 loading.style.display = "none";
 function Timer(){
