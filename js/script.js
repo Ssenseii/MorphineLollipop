db1bd7ca59ec01ac9f0cae74ef349f8d39a7d318
Simon Reinhardt <saadabou.study@gmail.com>
2022-07-04 15:21:13 +0100

    bubbly button

--- js/script.js
+++ js/script.js
@@ -12,3 +12,35 @@ function Timer(){
     loading.style.display = "initial";
 }
 
+/// button animation
+
+var animateButton = function (e) {
+
+    e.preventDefault;
+    //reset animation
+    e.target.classList.remove('animate');
+
+    e.target.classList.add('animate');
+    setTimeout(function () {
+        e.target.classList.remove('animate');
+    }, 700);
+};
+
+var bubblyButtons = document.getElementsByClassName("flash__text-button");
+
+for (var i = 0; i < bubblyButtons.length; i++) {
+    bubblyButtons[i].addEventListener('click', animateButton, false);
+}
+
+/// Enter
+
+$("#Enter").click(
+    function () {
+        console.log("clicked...waiting...");
+
+        setTimeout(
+            function () {
+                location.href = "https://github.com/Ssenseii/MorphineLollipop";
+            },
+            800);
+    });
