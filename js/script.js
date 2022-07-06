ae630ac18b4c5461b46e062abb047d132e961a0e
Simon Reinhardt <saadabou.study@gmail.com>
2022-07-06 14:59:18 +0100

    working search bar

--- js/script.js
+++ js/script.js
@@ -49,3 +49,25 @@ $("#Enter").click(
     });
 
 
+///Search function
+
+
+function search(){
+        // Declare variables
+        var input, filter, ul, li, a, i, txtValue;
+        input = document.getElementById('projectTitlesInput');
+        filter = input.value.toUpperCase();
+        ul = document.getElementById("projectTitles");
+        li = ul.getElementsByTagName('li');
+
+        // Loop through all list items, and hide those who don't match the search query
+        for (i = 0; i < li.length; i++) {
+            a = li[i].getElementsByTagName("a")[0];
+            txtValue = a.textContent || a.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                li[i].style.opacity = "1";
+            } else {
+                li[i].style.opacity = "0.5";
+            }
+        }
+}
